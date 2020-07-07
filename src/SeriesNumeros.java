f684d7e874421061d991bde66195a0a53124b52a
Victor <vgdobon91@gmail.com>
2020-07-07 10:07:16 -0400

    Proyecto del día07 en Fundamentos de Programación (07/07/2020) Se ha añadido un ejercicio de series de nº mediantes dos bucles (bidimensional).

---
+++ src/SeriesNumeros.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SeriesNumeros {
+//Pedir un nº y el programa debe de escribir una serie de lineas en las que empieza por 1 y va añadiendo un nº cada linea.
+    //1
+    //1 2
+    //1 2 3
+    //....
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un nº para realizar tantas filas de ese nº: ");
+        int num = sc.nextInt();
+
+
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
